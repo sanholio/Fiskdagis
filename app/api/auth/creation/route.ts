03674159625f16a2bf185500f8242838b65e924e
Alexander <sanholio90@gmail.com>
2025-02-16 21:29:00 +0100

    push

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://shoe-marshal.vercel.app/"
+      : "https://fiskdagis.vercel.app/"
   );
 }
