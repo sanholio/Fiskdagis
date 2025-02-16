beaf340708a14adcb81bd084cb516391b9403506
Alexander <sanholio90@gmail.com>
2025-02-16 21:18:27 +0100

    fix

--- app/actions.ts
+++ app/actions.ts
@@ -267,11 +267,11 @@ export async function checkOut() {
       success_url:
         process.env.NODE_ENV === "development"
           ? "http://localhost:3000/payment/success"
-          : "https://shoe-marshal.vercel.app/payment/success",
+          : "https://fiskdagis.vercel.app//payment/success",
       cancel_url:
         process.env.NODE_ENV === "development"
           ? "http://localhost:3000/payment/cancel"
-          : "https://shoe-marshal.vercel.app/payment/cancel",
+          : "https://fiskdagis.vercel.app//payment/cancel",
       metadata: {
         userId: user.id,
       },
